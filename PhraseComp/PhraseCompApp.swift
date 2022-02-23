77650d85165739689d72e1050eae65531c1517b0
Sophie Strausberg <sophie.strausberg@gmail.com>
2022-02-23 10:23:51 -0500

    Initial Commit

---
+++ PhraseComp/PhraseCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhraseCompApp.swift
+//  PhraseComp
+//
+//  Created by Sophie Strausberg on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct PhraseCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
